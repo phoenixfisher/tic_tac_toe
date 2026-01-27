8afd9a9cf96a008dc13ddcc7aa2a6dd221a01133
Phoenix Fisher <phoenixfisher702@gmail.com>
2026-01-26 22:22:24 -0700

    Instructions written out, not functional yet

---
+++ tic_tac_toe/Board.cs
@@ -0,0 +1,12 @@
+namespace tic_tac_toe;
+
+public class Board
+{
+    // Pair programmers:
+    // Remember: don't look at the other pair's code
+    // TODO:
+    // 1. Receive the "board" array from the driver class.
+    // 2. Contain a method that prints the board on the information passed to it.
+    // 3. Contain a method that receives the game board array as input and returns
+    // if there is a winner and who it was.
+}
---
+++ tic_tac_toe/Program.cs
@@ -0,0 +1,10 @@
+﻿// Pair programmers:
+// Remember: don't look at the other pair's code
+// TODO:
+// 1. Welcome the user to the game.
+// 2. Create a game board array to store the players' choices.
+// 3. Ask each player in turn for their choice and update the game board array.
+// 4. Print the board by calling the method in the supporting class (called "Board").
+// 5. Check for a winner by calling the method in the supporting class,
+// and notify the players when a win has occurred and which player won the game.
+Console.WriteLine("Hello, World!");
